88a48d8585c6618c6e8ba9c010636c0486151e25
Andreas Hirtenlehner <andreas.hirtenlehner@outlook.com>
2019-01-25 01:45:52 +0100

    add Keil project

---
+++ _Keil/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright:
+ * ----------------------------------------------------------------
+ * This confidential and proprietary software may be used only as
+ * authorised by a licensing agreement from ARM Limited
+ *   (C) COPYRIGHT 2014, 2016 ARM Limited
+ *       ALL RIGHTS RESERVED
+ * The entire notice above must be reproduced on all authorised
+ * copies and copies may only be made to the extent permitted
+ * by a licensing agreement from ARM Limited.
+ * ----------------------------------------------------------------
+ * File:     main.c
+ * ----------------------------------------------------------------
+ *
+ */
+ 
+/*
+ * --------Included Headers--------
+ */
+
+#include "mbed.h"
+
+/*******************************************************************/
+
+uint8_t LED_val = 0; // Value will be toggled for making the LED's Blink
+DigitalOut led0(LED0, 1);
+Thread thread;
+
+void led0_thread() {
+    while (true) {
+      led0 = !led0;
+			wait(1);
+    }
+}
+
+int main (void)
+{
+	thread.start(led0_thread);
+    
+  while ( 1 )
+  {
+
+  }
+}
